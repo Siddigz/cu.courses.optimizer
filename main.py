814b300424f46858b9978f0e7975377fba2150be
Siddigzmalik <80242309+Siddigzmalik@users.noreply.github.com>
2025-04-20 12:57:18 -0400

    commit 2

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ def main():
     parseText()
 
 def parseText():
-    text = open("input.txt",r)
+    text = open("input.txt","r").read()
     print(text)
+    for line in text:
+        pass
 
 
 
 
 
-if __main__ == "__name__":
+if __name__ == "__main__":
     main()
